25aec224d2012133f23fef8f5b3e6ea22542ac52
kimisuke <kimisuke0503@gmail.com>
2022-09-08 19:15:49 +0900

    blog_0908追加（仮）

--- js/move.js
+++ js/move.js
@@ -1,13 +1,7 @@
-const logo = document.querySelector("#js-logo");
 const btnMenu = document.querySelector("#js-btn-menu");
 const nav = document.querySelector("#js-menu");
-const backCover = document.querySelector("#js-back-cover");
 
-logo.addEventListener("click", function () {
-  logo.classList.toggle("active");
-});
 btnMenu.addEventListener("click", function () {
   btnMenu.classList.toggle("active");
   nav.classList.toggle("active");
-  backCover.classList.toggle("active");
 });
