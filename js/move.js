f6112f2eb4c6eb7536321dd5289eaa56dedf16bb
kimisuke <kimisuke0503@gmail.com>
2022-09-12 16:23:49 +0900

    ヘッダー変更と修正

--- js/move.js
+++ js/move.js
@@ -5,3 +5,17 @@ btnMenu.addEventListener("click", function () {
   btnMenu.classList.toggle("active");
   nav.classList.toggle("active");
 });
+
+$(window).on("scroll", function () {
+  if (170 /*kimisukeimgの高さ*/ < $(this).scrollTop()) {
+    $(".page-header").addClass("change-color");
+    $("a#js-logo").addClass("change-color");
+    $("#js-btn-menu > span").addClass("change-color");
+    $("#js-main-nav > li > a").addClass("change-color");
+  } else {
+    $(".page-header").removeClass("change-color");
+    $("a#js-logo").removeClass("change-color");
+    $("#js-btn-menu > span").removeClass("change-color");
+    $("#js-main-nav > li > a").removeClass("change-color");
+  }
+});
