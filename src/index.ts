096244995239c5a563ed97e4631ad26f4df35e92
kimisuke <kimisuke0503@gmail.com>
2022-09-13 17:33:18 +0900

    typescriptで書き直し

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
+import $ from "jquery";
+
+// ハンバーガーメニュー用
 const btnMenu: HTMLElement | null = document.getElementById("js-btn-menu");
 const subNav: HTMLElement | null = document.getElementById("js-menu");
-
 btnMenu?.addEventListener("click", () => {
   // ?はオプショナルチェイニングで、nullでもエラーにならずundefindが帰ってくるようにする
   btnMenu?.classList.toggle("active");
   subNav?.classList.toggle("active");
+});
+
+$(window).on("scroll", function () {
+  if (170 /*kimisukeimgの高さ*/ < $(this).scrollTop()) {
+    $(".page-header").addClass("change-color");
+    $("a#js-logo").addClass("change-color");
+    $("#js-btn-menu > span").addClass("change-color");
+    $("#js-main-nav > li > a").addClass("change-color");
+  } else {
+    $(".page-header").removeClass("change-color");
+    $("a#js-logo").removeClass("change-color");
+    $("#js-btn-menu > span").removeClass("change-color");
+    $("#js-main-nav > li > a").removeClass("change-color");
+  }
 });
